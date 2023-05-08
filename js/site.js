66f8929954130450d43af35a2210b39f88bf59e0
Anthony Stender <anthony.m.stender@gmail.com>
2023-05-08 17:19:56 -0500

    Cleaned up duplicate code, added E minutes

--- js/site.js
+++ js/site.js
@@ -11,6 +11,8 @@ function getWorkout() {
   if (workout.startsWith("SE")) {
     workout = workout.replace(/\d+x\d+/, `3x${10 * currentWeek}`);
     exercises.innerHTML = getSEExercises();
+  } else if (workout.startsWith("E")) {
+    exercises.innerHTML = "15-30 Minutes";
   }
   const workoutEl = document.getElementById("workout");
   workoutEl.textContent = workout;
@@ -32,7 +34,7 @@ function getWorkout() {
     "“A little progress each day adds up to big results.”",
     "“You may not be there yet, but you are closer than you were yesterday.”",
     "“Strong people are harder to kill than weak people and are more useful in general.” – Mark Rippetoe",
-    "“No man has the right to be an amateur in the matter of physical training. It is a shame for a man to grow old without seeing the beauty and strength of which his body is capable.” – Socrates"
+    "“No man has the right to be an amateur in the matter of physical training. It is a shame for a man to grow old without seeing the beauty and strength of which his body is capable.” – Socrates",
   ];
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
   const motivationEl = document.getElementById("motivation");
@@ -49,28 +51,6 @@ function getWorkout() {
   const imgEl = document.getElementById("motivation-img");
   imgEl.src = image;
 }
-
-const quotes = [
-  "“Success is not final, failure is not fatal: it is the courage to continue that counts.” – Winston Churchill",
-  "“Believe you can and you're halfway there.” – Theodore Roosevelt",
-  "“Strive not to be a success, but rather to be of value.” – Albert Einstein",
-  "“I have not failed. I've just found 10,000 ways that won't work.” – Thomas Edison",
-  "“The only way to do great work is to love what you do.” – Steve Jobs",
-];
-const quote = quotes[Math.floor(Math.random() * quotes.length)];
-const motivationEl = document.getElementById("motivation");
-motivationEl.textContent = quote;
-
-const images = [
-  "motivation1.jpg",
-  "motivation2.jpg",
-  "motivation3.jpg",
-  "motivation4.jpg",
-  "motivation5.jpg",
-];
-const image = images[Math.floor(Math.random() * images.length)];
-const imgEl = document.getElementById("motivation-img");
-imgEl.src = image;
 
 function getSEExercises() {
   return "Overhead Press, Bent Over Row, Squat, Romanian Deadlift, KB Swings, Sit Ups"; // Replace this with your list of exercises
